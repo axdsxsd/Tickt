40a10d527f177a912b66d4488c6f253aff20c898
Tap0kkk <tap0k.buk@gmail.com>
2025-12-03 14:47:59 +0300

    Отредактировал DATABASE_URL

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql+psycopg2://postgres:665267@192.168.88.15:5432/ticked"
+DATABASE_URL = "postgresql+psycopg2://postgres:bd0d1f64b11fd214aad6@31.97.154.186:51344/tickt"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
