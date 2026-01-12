32b728bf51913315de3965046d9ae9f10443e531
Tap0kkk <tap0k.buk@gmail.com>
2026-01-12 11:47:52 +0300

    Fix main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import todos, auth, users
-from .database import Base, engine
 from .config import settings
 from fastapi.staticfiles import StaticFiles
 from pathlib import Path
-
-
 
 app = FastAPI(
     title=settings.APP_NAME,
@@ -13,9 +11,31 @@ app = FastAPI(
     description=settings.APP_DESCRIPTION,
 )
 
+# ======================
+# Настройка CORS
+# ======================
+origins = [
+    "https://army-tickt-frontend.3zasdl.easypanel.host",  # твой продовый фронтенд
+    "http://localhost:5173",  # dev фронтенд локально (если нужно)
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # GET, POST, PUT, DELETE
+    allow_headers=["*"],  # все заголовки
+)
+
+# ======================
+# Static files
+# ======================
 BASE_DIR = Path(__file__).resolve().parent.parent
 app.mount("/images", StaticFiles(directory=BASE_DIR / "images"), name="images")
 
+# ======================
+# Routers
+# ======================
 app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(users.router)
